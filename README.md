11763c98bc974b494476a6e0f591aa3826a2f1ec
MrsPepa <jose.leon1996@gmail.com>
2018-02-13 18:32:50 -0300

    ajax api

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+
 ##### Para este ejercicio guiado queremos hacer una petición a la página del NY Times y que con la respuesta podamos ocupar sus noticias.
+
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,50 @@
 `use strict`
 
-const form = document.getElementById('search-form');
-const searchField = document.getElementById('search-keyword');
-const responseContainer = document.getElementById('response-container');
+const $form = $('#search-form');
+const $searchField = $('#search-keyword');
+const $responseContainer = $('#response-container');
 let searchedForText;
 
-form.addEventListener('submit', function(e) {
+$form.submit(function(e) {
   e.preventDefault();
-  responseContainer.innerHTML = '';
-  searchedForText = searchField.value;
+  $responseContainer.html('');
+  searchedForText = $searchField.val();
   getNews();
 });
 
 function getNews() {
-  const articleRequest = new XMLHttpRequest();
+  /*const articleRequest = new XMLHttpRequest();
   articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=79494dd65fee4ccbbf00b1fff80bccd4`);
   articleRequest.onload = addNews;
   articleRequest.onerror= handleError;
-  articleRequest.send();
+  articleRequest.send();*/
+  $.ajax({
+    url: `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=79494dd65fee4ccbbf00b1fff80bccd4`
+  }).done(addNews)
+  .fail(handleError);
 }
-function handleError() {
-  console.log('Se ha presentado un error');
-}
-function addNews() {
-  const data = JSON.parse(this.responseText);
+
+function addNews(news) {
+  console.log(news);
+  const articles = news.response.docs[0];
+
+  articles.forEach(function(article){
+    const title = article.headline.main;
+    const snippet = article.snippet;
+
+    let $li = $('<li />').addClass('articleClass').text(snippet);
+
+    $responseContainer.append($li);
+  });
+
+  /*const title = article.headline.main;
+  const snippet = article.snippet;
+
+  let $li = $('<li />').addClass('articleClass').text(snippet);
+
+  $responseContainer.append($li);*/
+
+  /*const data = JSON.parse(this.responseText);
   const article = data.response.docs[0];
   const title = article.headline.main;
   const snippet = article.snippet;
@@ -32,5 +53,9 @@ function addNews() {
   li.className = 'articleClass';
   li.innerText = snippet;
 
-  responseContainer.appendChild(li);
+  responseContainer.appendChild(li);*/
+}
+
+function handleError() {
+  console.log('Se ha presentado un error');
 }
