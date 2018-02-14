c2521f5c5cd6e70656eff14480bba0b9642908a5
MrsPepa <jose.leon1996@gmail.com>
2018-02-14 14:37:38 -0300

    funcion para recorrer data con jQuery y EMS6

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,46 +1,45 @@
 `use strict`
 
-const $form = $('#search-form');
+/*const $form = $('#search-form');
 const $searchField = $('#search-keyword');
 const $responseContainer = $('#response-container');
-let searchedForText;
-
-/*const form = document.getElementById('search-form');
-const searchField = document.getElementById('search-keyword');
-const responseContainer = document.getElementById('response-container');
 let searchedForText;*/
 
-$form.submit(function(e) {
-  e.preventDefault();
-  $responseContainer.html('');
-  searchedForText = $searchField.val();
-  getNews();
+const form = document.getElementById('search-form');
+const searchField = document.getElementById('search-keyword');
+const responseContainer = document.getElementById('response-container');
+let searchedForText;
 
-/*form.addEventListener('submit', function(e) {
+/*$form.submit(function(e) {
   e.preventDefault();
   $responseContainer.html('');
   searchedForText = $searchField.val();
   getNews();
 });*/
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  responseContainer.innerHTML = '';
+  searchedForText = searchField.value;
+  getNews();
 });
 
-
 function getNews() {
-  $.ajax({
+  /*$.ajax({
     url: `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=79494dd65fee4ccbbf00b1fff80bccd4`
   }).done(addNews)
-  .fail(handleError);
+  .fail(handleError);*/
 
-  /*const articleRequest = new XMLHttpRequest();
+  const articleRequest = new XMLHttpRequest();
   articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=79494dd65fee4ccbbf00b1fff80bccd4`);
   articleRequest.onload = addNews;
   articleRequest.onerror= handleError;
-  articleRequest.send();*/
+  articleRequest.send();
 }
 
-function addNews(news) {
-  console.log(news);
-  const articles = news.response.docs[0];
+function addNews() {
+  /*console.log(news);
+  const articles = news.response.docs;
 
   articles.forEach(function(article){
     const title = article.headline.main;
@@ -49,18 +48,21 @@ function addNews(news) {
     let $li = $('<li />').addClass('articleClass').text(snippet);
 
     $responseContainer.append($li);
-  });
+  });*/
 
-  /*const data = JSON.parse(this.responseText);
-  const article = data.response.docs[0];
-  const title = article.headline.main;
-  const snippet = article.snippet;
+  const data = JSON.parse(this.responseText);
+  const articles = data.response.docs;
 
-  let li = document.createElement('li');
-  li.className = 'articleClass';
-  li.innerText = snippet;
+  articles.forEach(function(article){
+    const title = article.headline.main;
+    const snippet = article.snippet;
 
-  responseContainer.appendChild(li);*/
+    let li = document.createElement('li');
+    li.className = 'articleClass';
+    li.innerText = snippet;
+
+    responseContainer.appendChild(li);
+  })
 }
 
 function handleError() {
