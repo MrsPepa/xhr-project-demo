e6d59de879e155c82ea1ec422ddba6b951baa732
MrsPepa <jose.leon1996@gmail.com>
2018-02-13 22:09:19 -0300

    implementando AJAX

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,52 @@
 `use strict`
 
-const form = document.getElementById('search-form');
+const $form = $('#search-form');
+const $searchField = $('#search-keyword');
+const $responseContainer = $('#response-container');
+let searchedForText;
+/*const form = document.getElementById('search-form');
 const searchField = document.getElementById('search-keyword');
 const responseContainer = document.getElementById('response-container');
-let searchedForText;
+let searchedForText;*/
 
-form.addEventListener('submit', function(e) {
+$form.submit(function(e) {
+  e.preventDefault();
+  $responseContainer.html('');
+  searchedForText = $searchField.val();
+  getNews();
+});
+/*form.addEventListener('submit', function(e) {
   e.preventDefault();
   responseContainer.innerHTML = '';
   searchedForText = searchField.value;
   getNews();
-});
+});*/
 
 function getNews() {
-  const articleRequest = new XMLHttpRequest();
+  $.ajax({
+    url: `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=79494dd65fee4ccbbf00b1fff80bccd4`
+  }).done(addNews)
+  .fail(handleError);
+  /*const articleRequest = new XMLHttpRequest();
   articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=79494dd65fee4ccbbf00b1fff80bccd4`);
   articleRequest.onload = addNews;
   articleRequest.onerror= handleError;
-  articleRequest.send();
+  articleRequest.send();*/
 }
-function handleError() {
-  console.log('Se ha presentado un error');
-}
-function addNews() {
-  const data = JSON.parse(this.responseText);
+
+function addNews(news) {
+  console.log(news);
+  const articles = news.response.docs[0];
+
+  articles.forEach(function(article) {
+    const title = article.headline.main;
+    const snippet = article.snippet;
+
+    let $li = $('<li />').addClass('articleClass').text(title);
+    $responseContainer.append($li);
+  });
+  
+  /*const data = JSON.parse(this.responseText);
   const article = data.response.docs[0];
   const title = article.headline.main;
   const snippet = article.snippet;
@@ -32,5 +55,8 @@ function addNews() {
   li.className = 'articleClass';
   li.innerText = snippet;
 
-  responseContainer.appendChild(li);
+  responseContainer.appendChild(li);*/
+}
+function handleError() {
+  console.log('Se ha presentado un error');
 }
